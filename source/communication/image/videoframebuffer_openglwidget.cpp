570d42c4a4d30cf6f4b5457aead21d5db57b4766
DahliaEnterprise <dahliacommunication67@gmail.com>
2019-04-24 18:04:12 -0700

    Merged code lines to lowQuality extraction function.

--- source/communication/image/videoframebuffer_openglwidget.cpp
+++ source/communication/image/videoframebuffer_openglwidget.cpp
@@ -15,34 +15,63 @@ QImage videoframebuffer_openglwidget::splitFrame(QVideoFrame VideoFrame)
     VideoFrame.map(QAbstractVideoBuffer::ReadOnly);
     QImage::Format imageFormat = QVideoFrame::imageFormatFromPixelFormat(VideoFrame.pixelFormat());
     QImage VideoFrameAsImage = QImage(VideoFrame.bits(), VideoFrame.width(), VideoFrame.height(), VideoFrame.bytesPerLine(), imageFormat);
-    QImage lowQualityFrameImage = QImage(1280, 720, imageFormat);
-    QPainter painter(&lowQualityFrameImage);
 
     //Low Quality frame
+    QImage lowQualityFrameImage = QImage(1280, 720, QImage::Format_RGB32);
+    lowQualityFrameImage = this->lowQuality(lowQualityFrameImage, VideoFrameAsImage);
+
+
+    //Medium Quality Frame
+    QImage medQualityFrameImage = QImage(1280, 720, QImage::Format_ARGB32);
+    QPainter painter2(&medQualityFrameImage);
+    QPen transparentPen; transparentPen.setStyle(Qt::SolidLine); transparentPen.setColor(QColor(0,0,0,0));
+    painter2.setPen(transparentPen);
+    painter2.fillRect(0,0,1280,720,QColor(0,0,0,0));
+
+
+
+    return this->temp_mergeframes(lowQualityFrameImage, medQualityFrameImage);
+}
+
+QImage videoframebuffer_openglwidget::lowQuality(QImage blank, QImage details)
+{
+    QPainter painter(&blank);
     int currentX = 0;
     int currentY = 0;
-    int lowSquareWidthHeight = 8;
+    int lowSquareWidthHeight = 16;
     bool keep_looping = true;
     while(keep_looping == true)
     {
         QPen pen;
         pen.setStyle(Qt::SolidLine);
-        pen.setColor(VideoFrameAsImage.pixel(currentX,currentY));
+        pen.setColor(details.pixel(currentX,currentY));
         painter.setPen(pen);
-        painter.fillRect(currentX,currentY,8,8,VideoFrameAsImage.pixel(currentX,currentY));
+        painter.fillRect(currentX,currentY,1,1,details.pixel(currentX,currentY));
 
         currentX += lowSquareWidthHeight;
-        if(currentX > VideoFrameAsImage.width() - 1)
+        if(currentX > details.width() - 1)
         {
             currentX = 0;
             currentY += lowSquareWidthHeight;
         }
 
-        if(currentY > VideoFrameAsImage.height() - 1)
+        if(currentY > details.height() - 1)
         {
             keep_looping = false;
         }
     }
 
-    return lowQualityFrameImage;
+    return blank;
+}
+
+
+QImage videoframebuffer_openglwidget::temp_mergeframes(QImage lowQuality, QImage medQuality)
+{
+    QImage output = lowQuality;
+
+    QPainter painter(&output);
+    painter.drawImage(0,0,medQuality);
+
+
+    return output;
 }
--- source/communication/image/videoframebuffer_openglwidget.h
+++ source/communication/image/videoframebuffer_openglwidget.h
@@ -5,12 +5,18 @@
 #include <QOpenGLWidget>
 #include <QVideoFrame>
 #include <QPainter>
+#include <QDebug>
+#include <QImage>
 class videoframebuffer_openglwidget : public QOpenGLWidget
 {
 public:
     videoframebuffer_openglwidget();
     void start();
     QImage splitFrame(QVideoFrame VideoFrame);
+    QImage temp_mergeframes(QImage lowQuality, QImage medQuality);
+
+private:
+    QImage lowQuality(QImage blank, QImage details);
 
 };
 
